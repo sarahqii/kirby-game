3b0d49294c0312cada41bbaf95af7075a9beb84a
Khanh <112164825+khanhdo05@users.noreply.github.com>
2023-11-24 16:05:44 -0600

    define function ball

--- object-images.scm
+++ object-images.scm
@@ -3,3 +3,16 @@
 ;;; This file includes all the static images needed for the game.
 
 (import image)
+
+;;; OBJECTS DRAWING
+
+;;; (ball length main-color shadow-color) -> drawing?
+;;;   length: integer?, non-negative
+;;;   main-color: string?, or predefined rgb color.
+;;;  shadow-color: string?, or predefined rgb color.
+;;; Draws a round object as a base image to make different objects for Kirby to eat.
+(define ball
+  (lambda (length main-color shadow-color)
+    (overlay/offset -3 -6 
+                    (circle (* length 0.85) "solid" main-color)
+                    (circle length "solid" shadow-color))))
